1cc78e4fa2996030ec1b3478e74c6dde07652bb3
Francisco Oliveira <franciscodiasoliveirapt@gmail.com>
2023-09-06 17:57:47 +0100

    Nav Admin Fetch User Name and Surname

--- admin/includes/nav-top.php
+++ admin/includes/nav-top.php
@@ -2,17 +2,36 @@
                     <div class="container-fluid"><button class="btn btn-link d-md-none rounded-circle me-3" id="sidebarToggleTop" type="button"><i class="fas fa-bars"></i></button>
                         <ul class="navbar-nav flex-nowrap ms-auto">
                             <li class="nav-item dropdown d-sm-none no-arrow"><a class="dropdown-toggle nav-link" aria-expanded="false" data-bs-toggle="dropdown" href="#"><i class="fas fa-search"></i></a>
-                                <div class="dropdown-menu dropdown-menu-end p-3 animated--grow-in" aria-labelledby="searchDropdown">
-                                    <form class="me-auto navbar-search w-100">
-                                        <div class="input-group"><input class="bg-light form-control border-0 small" type="text" placeholder="Search for ...">
-                                            <div class="input-group-append"><button class="btn btn-primary py-0" type="button"><i class="fas fa-search"></i></button></div>
-                                        </div>
-                                    </form>
-                                </div>
-                            </li>
+                            </li> 
                             <li class="nav-item dropdown no-arrow">
-                                <div class="nav-item dropdown no-arrow"><a class="dropdown-toggle nav-link" aria-expanded="false" data-bs-toggle="dropdown" href="#"><span class="d-none d-lg-inline me-2 text-gray-600 small">Francisco Oliveira</span></a>
-                                    <div class="dropdown-menu shadow dropdown-menu-end animated--grow-in"><a class="dropdown-item" href="#"><i class="fas fa-user fa-sm fa-fw me-2 text-gray-400"></i>&nbsp;Profile</a><a class="dropdown-item" href="#"><i class="fas fa-cogs fa-sm fa-fw me-2 text-gray-400"></i>&nbsp;Settings</a><a class="dropdown-item" href="#"><i class="fas fa-list fa-sm fa-fw me-2 text-gray-400"></i>&nbsp;Activity log</a>
+                               <div class="nav-item dropdown no-arrow"> <a class="dropdown-toggle nav-link" aria-expanded="false" data-bs-toggle="dropdown" href="#"><span class="d-none d-lg-inline me-2 text-gray-600 small">
+                               <?php
+                                if (isset($_SESSION['email'])) {
+                                    // Include the database configuration
+                                    include('../config.php');
+
+                                    // Get the logged-in user's email from the session
+                                    $email = $_SESSION['email'];
+
+                                    // Query to fetch the user's name and surname based on their email
+                                    $query = "SELECT name, surname FROM users WHERE email = '$email'";
+                                    $result = $conn->query($query);
+
+                                    if ($result->num_rows == 1) {
+                                        $row = $result->fetch_assoc();
+                                        $firstName = $row['name']; // Store 'name' as first name
+                                        $lastName = $row['surname']; // Store 'surname' as last name
+
+                                        // Display the user's first and last name
+                                        echo "Welcome, $firstName $lastName!";
+                                    } else {
+                                        // User not found in the database, handle accordingly
+                                        echo "User not found.";
+                                    }
+                                }
+                                    ?>
+                                </span></a>
+                                    <div class="dropdown-menu shadow dropdown-menu-end animated--grow-in">
                                         <div class="dropdown-divider"></div><a class="dropdown-item" href="includes/logout.php"><i class="fas fa-sign-out-alt fa-sm fa-fw me-2 text-gray-400"></i>&nbsp;Logout</a>
                                     </div>
                                 </div>
