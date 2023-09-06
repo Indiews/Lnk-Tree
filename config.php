281e66eef2263d60a4f475ce76b4c0e02f547fd8
Francisco Oliveira <franciscodiasoliveirapt@gmail.com>
2023-09-06 17:33:07 +0100

    Email Server Config Update

--- config.php
+++ config.php
@@ -5,6 +5,13 @@ $db_user = "itsfdo_its";
 $db_password = "qyvJgtfOBO";
 $db_name = "itsfdo_its";
 
+// Email server configuration
+$email_host = "smtp.example.com"; // Your SMTP server hostname
+$email_port = 587; // Port for SMTP (usually 587 for TLS/STARTTLS)
+$email_username = "your_email@example.com"; // Your email username
+$email_password = "your_email_password"; // Your email password
+$email_sender = "your_email@example.com"; // The email address to be used as the sender
+
 // Create a database connection
 $conn = new mysqli($db_host, $db_user, $db_password, $db_name);
 
