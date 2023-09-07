a5617db0c1f836a00aa2cf71d76602d8b1bdbb74
Francisco Oliveira <franciscodiasoliveirapt@gmail.com>
2023-09-07 16:59:39 +0100

    User Info System
    This system currently has a bug where it will submit both forms at the same time. Need to fix

--- admin/profile.php
+++ admin/profile.php
@@ -33,28 +33,105 @@
                             <p class="text-primary m-0 fw-bold">User Settings</p>
                         </div>
 
-                        
+                        <?php
+// Include the database configuration and other necessary files
+include('../config.php');
 
-                        <div class="card-body">
-                            <form>
-                                <div class="row">
-                                    <div class="col">
-                                        <div class="mb-3"><label class="form-label" for="first_name"><strong>First Name</strong></label><input class="form-control" type="text" id="first_name" placeholder="John" name="first_name"></div>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col">
-                                        <div class="mb-3"><label class="form-label" for="last_name"><strong>Last Name</strong></label><input class="form-control" type="text" id="last_name-1" placeholder="Doe" name="last_name"></div>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col">
-                                        <div class="mb-3"><label class="form-label" for="email"><strong>Email Address</strong></label><input class="form-control" type="email" id="email-1" placeholder="user@example.com" name="email"></div>
-                                    </div>
-                                </div>
-                                <div class="mb-3"><button class="btn btn-primary btn-sm" type="submit">Save Settings</button></div>
-                            </form>
-                        </div>
+// Initialize variables for error messages
+$firstName = $lastName = $email = '';
+$emailError = '';
+$successMessageProf = '';
+
+// Fetch user's email from the session
+if (isset($_SESSION['email'])) {
+    $userEmail = $_SESSION['email'];
+
+    // Fetch user data based on their email
+    $sql = "SELECT * FROM users WHERE email = '$userEmail'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows == 1) {
+        $row = $result->fetch_assoc();
+        $firstName = $row['name'];
+        $lastName = $row['surname'];
+        $email = $row['email'];
+    }
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Get updated user data from the form
+    $newFirstName = $_POST['first_name'];
+    $newLastName = $_POST['last_name'];
+    $newEmail = $_POST['email'];
+
+    // Check if the new email is the same as the current email or already exists
+    if ($newEmail !== $userEmail) {
+        $checkEmailSql = "SELECT email FROM users WHERE email = '$newEmail'";
+        $checkResult = $conn->query($checkEmailSql);
+
+        if ($checkResult->num_rows > 0) {
+            $emailError = "A profile with this email already exists.";
+        }
+    }
+
+    // If no email error, proceed with the update
+    if (empty($emailError)) {
+        // Update the email in the session if it has changed
+        if ($newEmail !== $userEmail) {
+            $_SESSION['email'] = $newEmail;
+        }
+
+        // Update the user's data in the database
+        $updateSql = "UPDATE users SET name = '$newFirstName', surname = '$newLastName', email = '$newEmail' WHERE email = '$userEmail'";
+
+        if ($conn->query($updateSql) === TRUE) {
+            // Data updated successfully, set a success message
+            $successMessageProf = "Settings saved successfully.";
+
+            // Update the displayed user data with the new values
+            $firstName = $newFirstName;
+            $lastName = $newLastName;
+            $email = $newEmail;
+        } else {
+            // Data update failed, display an error message
+            echo "Error updating data: " . $conn->error;
+        }
+    }
+}
+?>
+
+<!-- Your HTML code for displaying user data and editing form -->
+<div class="card-body">
+    <?php
+    // Display a success message if settings are saved successfully
+    if (!empty($successMessageProf)) {
+        echo '<div class="alert alert-success">' . $successMessageProf . '</div>';
+    }
+
+    // Display an email error message if one exists
+    if (!empty($emailError)) {
+        echo '<div class="alert alert-danger">' . $emailError . '</div>';
+    }
+    ?>
+    <form method="POST" action="">
+        <div class="row">
+            <div class="col">
+                <div class="mb-3"><label class="form-label" for="first_name"><strong>First Name</strong></label><input class="form-control" type="text" id="first_name" placeholder="John" name="first_name" value="<?php echo $firstName; ?>"></div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <div class="mb-3"><label class="form-label" for="last_name"><strong>Last Name</strong></label><input class="form-control" type="text" id="last_name" placeholder="Doe" name="last_name" value="<?php echo $lastName; ?>"></div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <div class="mb-3"><label class="form-label" for="email"><strong>Email Address</strong></label><input class="form-control" type="email" id="email" placeholder="user@example.com" name="email" value="<?php echo $email; ?>"></div>
+            </div>
+        </div>
+        <div class="mb-3"><button class="btn btn-primary btn-sm" type="submit">Save Settings</button></div>
+    </form>
+</div>
                     </div>
                     <div class="card shadow mb-3">
                         <div class="card-header py-3">
@@ -106,7 +183,7 @@
 
                                     if ($conn->query($updateSql) === TRUE) {
                                         // Password updated successfully, you can redirect or display a success message
-                                        $successMessage = "Password updated successfully.";
+                                        $successMessagePass = "Password updated successfully."; // Updated variable name
                                     } else {
                                         // Password update failed, display an error message
                                         echo "Error updating password: " . $conn->error;
@@ -119,14 +196,15 @@
                         }
                         ?>
 
+
                         <div class="card-body">
                             <form method="POST" action="">
                             <div class="mb-3">
                                 <div id="success-message">
                                 <?php
                                 // Check if the success message variable is set and display it
-                                if (isset($successMessage)) {
-                                    echo '<div class="alert alert-success">' . $successMessage . '</div>';
+                                if (isset($successMessagePass)) {
+                                    echo '<div class="alert alert-success">' . $successMessagePass . '</div>';
                                 }
                                 ?>
                                 </div>
