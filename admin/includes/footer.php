f56cf710f61e5c80eeda664896b5a65a2ac8bd2d
Francisco Oliveira <franciscodiasoliveirapt@gmail.com>
2023-09-11 19:37:31 +0100

    Update Footer

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -1,5 +1,5 @@
             <footer class="bg-white sticky-footer">
                 <div class="container my-auto">
-                    <div class="text-center my-auto copyright"><span>Copyright © Indiews - Digital Agency</span></div>
+                    <div class="text-center my-auto copyright"><span>Copyright © Indiews - Digital Agency<br></br>Made with ❤ in Portugal</span></div>
                 </div>
             </footer>
