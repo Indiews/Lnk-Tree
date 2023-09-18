299770782131595645d4ed3cdb55e48bcb3e0d78
Francisco Oliveira <franciscodiasoliveirapt@gmail.com>
2023-09-18 21:33:35 +0100

    Update to Indiews CDN

--- admin/login.php
+++ admin/login.php
@@ -28,7 +28,7 @@ if (isset($_SESSION['email'])) {
                     <div class="card-body p-0">
                         <div class="row">
                             <div class="col-lg-6 d-none d-lg-flex">
-                                <div class="flex-grow-1 bg-login-image" style="background-image: url(&quot;https://img.freepik.com/free-vector/mobile-login-concept-illustration_114360-83.jpg&quot;);"></div>
+                                <div class="flex-grow-1 bg-login-image" style="background-image: url(https://cdn.indiews.com/lnk-tree/branding/001.png);"></div>
                             </div>
                             <div class="col-lg-6">
                                 <div class="p-5">
