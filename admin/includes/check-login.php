823169ca9507a6783b927f3f572a78b99550973e
Francisco Oliveira <franciscodiasoliveirapt@gmail.com>
2023-09-18 17:05:44 +0100

    Check system user removed

--- admin/includes/check-login.php
+++ admin/includes/check-login.php
@@ -2,11 +2,36 @@
 session_start();
 
 // Check if the user is not logged in
-if (!isset($_SESSION['email'])) {
+/*if (!isset($_SESSION['email'])) {
     // Redirect to the login page
     header("Location: login.php"); // Change to the actual login page URL
     exit(); // Ensure that the script stops executing
+}*/
+
+// Include the database configuration
+include('../config.php');
+
+
+if (!isset($_SESSION['email'])) {
+    // Redirect to the login page
+    header("Location: login.php");}
+else {
+// Get the email from the session
+$email = $_SESSION['email'];
+
+// Query the database to check if the email exists
+ $query = "SELECT * FROM users WHERE email = '$email'";
+$result = $conn->query($query);
+
+// Check if the email exists in the database
+if ($result->num_rows === 0) {
+    // Email not found in the database, redirect to the login page
+    header("Location: login.php"); 
+   
 }
 
-// If the user is logged in, you can proceed with displaying the protected content.
+}
+exit();
+
+// If the user is logged in and the email exists in the database, you can proceed with displaying the protected content.
 ?>
