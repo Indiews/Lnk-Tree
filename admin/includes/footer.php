57bdf10a4456323eeb9dea5066d79872877dbb6e
Francisco Oliveira <franciscodiasoliveirapt@gmail.com>
2023-09-22 02:21:08 +0100

    V1

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -1,5 +1,5 @@
 <footer class="bg-white sticky-footer">
     <div class="container my-auto">
-        <div class="text-center my-auto copyright"><span>Copyright © Indiews - Digital Agency<br></br><strong><img style="height:20px" src="https://cdn.indiews.com/lnk-tree/icons/branch.png"> V 0.8.1</strong></span></div>
+        <div class="text-center my-auto copyright"><span>Copyright © Indiews - Digital Agency<br></br><strong><img style="height:20px" src="https://cdn.indiews.com/lnk-tree/icons/branch.png"> V 1.0.0</strong></span></div>
     </div>
 </footer>
